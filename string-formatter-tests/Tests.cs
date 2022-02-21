8b1332b3fdaf9d44c6873d0d2b09efc62bf07d5b
SunHowe <tosunhowe@gmail.com>
2022-02-21 11:03:03 +0800

    string formatter

---
+++ string-formatter-tests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using StringFormatter;
+
+namespace string_formatter_tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SimpleFormatTest()
+        {
+            Assert.AreEqual(StringEx.Format("{0}", "param"), "param");
+        }
+
+        [Test]
+        public void RuleFormatTest()
+        {
+            StringEx.RegisterFormatFunc("name", () => "玩家A");
+            Assert.AreEqual(StringEx.Format("{name}"), "玩家A");
+        }
+
+        [Test]
+        public void RuleFormatWithParametersTest()
+        {
+            StringEx.RegisterFormatFunc("i18n", (index, parameters) => $"文本内容({parameters[index]})");
+            
+            Assert.AreEqual(StringEx.Format("{0:i18n}", "key_1001"), "文本内容(key_1001)");
+        }
+
+        [Test]
+        public void SpecialCharTest()
+        {
+            Assert.AreEqual(StringEx.Format("{0}{{0}}", "param"), "param{0}");
+        }
+
+        [Test]
+        public void ComplexFormatTest()
+        {
+            StringEx.RegisterFormatFunc("name", () => "玩家A");
+            StringEx.RegisterFormatFunc("i18n", (index, parameters) => $"文本内容({parameters[index]})");
+            
+            Assert.AreEqual(StringEx.Format("{name}说了一句:\"{1:i18n}\", 手上拿着{0}", "一把刀", "key_1001"), 
+                "玩家A说了一句:\"文本内容(key_1001)\", 手上拿着一把刀");
+        }
+    }
+}
---
+++ string-formatter/StringEx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringFormatter
+{
+    public delegate string StringFormatFunc();
+
+    public delegate string StringFormatWithParametersFunc(int index, params object[] parameters);
+
+    public static class StringEx
+    {
+        private static readonly Dictionary<string, StringFormatFunc> _formatFuncs = new Dictionary<string, StringFormatFunc>();
+        private static readonly Dictionary<string, StringFormatWithParametersFunc> _formatWithParametersFuncs = new Dictionary<string, StringFormatWithParametersFunc>();
+
+        private static readonly Queue<StringBuilder> _sbPools = new Queue<StringBuilder>();
+
+        /// <summary>
+        /// 注册字符串格式化自定义规则的处理器
+        /// </summary>
+        public static void RegisterFormatFunc(string rule, StringFormatFunc func)
+        {
+            _formatFuncs[rule] = func;
+        }
+
+        /// <summary>
+        /// 注册带参数处理的字符串格式化自定义规则的处理器
+        /// </summary>
+        public static void RegisterFormatFunc(string rule, StringFormatWithParametersFunc func)
+        {
+            _formatWithParametersFuncs[rule] = func;
+        }
+
+        public static string Format(string format, params object[] parameters)
+        {
+            const char LeftChar = '{';
+            const char RightChar = '}';
+            const char SplitChar = ':';
+
+            var sb = GetStringBuilder();
+
+            // 文本内容{0}，{1}，去除特殊符号{{}}
+            var strLength = format.Length;
+            int pos1 = 0, pos2, pos3;
+            int paramIndex;
+            string tag;
+
+            while ((pos2 = format.IndexOf(LeftChar, pos1)) != -1)
+            {
+                // pos1 -> pos2 文本内容
+                if (pos1 < pos2)
+                    sb.Append(format.Substring(pos1, pos2 - pos1));
+
+                // next char {{
+                pos3 = pos2 + 1;
+                if (pos3 < strLength && format[pos3] == LeftChar)
+                {
+                    sb.Append(LeftChar);
+                    pos1 = pos3 + 1;
+                    continue;
+                }
+
+                pos1 = pos2 + 1;
+
+                pos2 = format.IndexOf(RightChar, pos1);
+                if (pos2 == -1 || pos2 == pos1)
+                    throw new FormatException("Input string was not in a correct format.");
+
+                pos3 = format.IndexOf(SplitChar, pos1);
+                if (pos3 == -1 || pos3 > pos2)
+                {
+                    // {tag}
+                    tag = format.Substring(pos1, pos2 - pos1);
+
+                    if (int.TryParse(tag, out paramIndex))
+                    {
+                        // {0}
+                        if (parameters == null || parameters.Length <= paramIndex)
+                            throw new FormatException("Index (zero based) must be greater than or equal to zero and less than the size of the argument list.");
+
+                        sb.Append(parameters[paramIndex]);
+                    }
+                    else
+                    {
+                        // {tag}
+                        if (!_formatFuncs.TryGetValue(tag, out var func))
+                            throw new FormatException($"Tag {tag} format func is missing");
+
+                        sb.Append(func.Invoke());
+                    }
+                }
+                else
+                {
+                    // {1:tag}
+                    tag = format.Substring(pos1, pos3 - pos1);
+                    
+                    if (!int.TryParse(tag, out paramIndex))
+                        throw new FormatException("Input string was not in a correct format.");
+                    
+                    if (parameters == null || parameters.Length <= paramIndex)
+                        throw new FormatException("Index (zero based) must be greater than or equal to zero and less than the size of the argument list.");
+
+                    tag = format.Substring(pos3 + 1, pos2 - pos3 - 1);
+                    
+                    if (!_formatWithParametersFuncs.TryGetValue(tag, out var func))
+                        throw new FormatException($"Tag {tag} format func with parameter is missing");
+
+                    sb.Append(func.Invoke(paramIndex, parameters));
+                }
+
+                pos1 = pos2 + 1;
+            }
+
+            while ((pos2 = format.IndexOf(RightChar, pos1)) != -1)
+            {
+                // pos1 -> pos2 文本内容
+                if (pos1 < pos2)
+                    sb.Append(format.Substring(pos1, pos2 - pos1));
+
+                // next char }}
+                pos3 = pos2 + 1;
+                if (pos3 < strLength && format[pos3] == RightChar)
+                {
+                    sb.Append(RightChar);
+                    pos1 = pos3 + 1;
+                    continue;
+                }
+
+                throw new FormatException("Input string was not in a correct format.");
+            }
+
+            if (pos1 == 0)
+            {
+                ReleaseStringBuilder(sb);
+                return format;
+            }
+
+            if (pos1 < strLength)
+                sb.Append(format.Substring(pos1, strLength - pos1));
+
+            var str = sb.ToString();
+            ReleaseStringBuilder(sb);
+            return str;
+        }
+
+        private static StringBuilder GetStringBuilder()
+        {
+            if (_sbPools.Count == 0)
+                return new StringBuilder();
+
+            var sb = _sbPools.Dequeue();
+            sb.Clear();
+            return sb;
+        }
+
+        private static void ReleaseStringBuilder(StringBuilder sb)
+        {
+            _sbPools.Enqueue(sb);
+        }
+    }
+}
